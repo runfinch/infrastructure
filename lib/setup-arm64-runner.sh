c1490effdc47bc49b3f6139f9e64af46a928e992
Weike Qu <weikequ@amazon.com>
2023-03-08 14:21:49 -0800

    Updated prod stage for 4 more ventura runners
    Signed-off-by: Weike Qu <weikequ@amazon.com>

--- lib/setup-arm64-runner.sh
+++ lib/setup-arm64-runner.sh
@@ -4,7 +4,7 @@
 HOMEDIR="/Users/ec2-user"
 RUNNER_DIR="$HOMEDIR/ar"
 mkdir -p $RUNNER_DIR && cd $RUNNER_DIR
-curl -o actions-runner-osx-arm64-2.298.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-osx-arm64-2.298.2.tar.gz
-echo "e124418a44139b4b80a7b732cfbaee7ef5d2f10eab6bcb3fd67d5541493aa971  actions-runner-osx-arm64-2.298.2.tar.gz" | shasum -a 256 -c
+curl -o actions-runner-osx-arm64-2.302.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.302.1/actions-runner-osx-arm64-2.302.1.tar.gz
+echo "f78f4db37bb7ba80e6123cec0e3984d1f2bb3f8f3a16db679c42ef830e0981d3  actions-runner-osx-arm64-2.302.1.tar.gz" | shasum -a 256 -c
 # Extract the installer
-tar xzf ./actions-runner-osx-arm64-2.298.2.tar.gz
+tar xzf ./actions-runner-osx-arm64-2.302.1.tar.gz
