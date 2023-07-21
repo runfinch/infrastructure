613dca5df99c7dda853b9c3b0f1ac88a4d565b71
Gavin Inglis <43075615+ginglis13@users.noreply.github.com>
2023-07-21 15:02:00 -0700

    add ECR repo for finch rootfs image (#355)
    Add an ECR repo to Finch infrastructure. this repository is where the
    image used as the basis for the Finch VM rootfs will live.
    
    to support Finch on Windows, we will be using WSL2.
    WSL2 differs from the Finch on Mac qemu/vz implementation in that WSL2
    provides a kernel and requires just a root filesystem to boot a custom
    distribution. We will be maintaining the root filesystem as an image
    in this ECR repository. Subsequent PRs to finch-core will include the
    Dockerfile, actions for exporting the image as an archive and uploading
    to the Finch dependencies S3 bucket.
    
    For now, the ECR repository is private to limit access controls to the
    GitHub action responsible for interacting with the image. However if in
    the future that proves to be cumbersome for developers, we can make the
    repository public for image pull and limit push permissions to the action.
    
    Signed-off-by: Gavin Inglis <giinglis@amazon.com>

---
+++ lib/ecr-repo-stack.ts
@@ -0,0 +1,29 @@
+import * as cdk from 'aws-cdk-lib';
+import { CfnOutput } from 'aws-cdk-lib';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import { Construct } from 'constructs';
+
+export class ECRRepositoryStack extends cdk.Stack {
+  public readonly repositoryOutput: CfnOutput;
+  public readonly repository: ecr.Repository;
+  constructor(scope: Construct, id: string, stage: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    const repoName = `finch-rootfs-image-${stage.toLowerCase()}`;
+    const ecrRepository = new ecr.Repository(this, 'finch-rootfs', {
+        repositoryName:repoName,
+        imageTagMutability: ecr.TagMutability.IMMUTABLE,
+        // TODO: CFN does not provide APIs for enhanced image scanning. 
+        // To address, create a custom stack that uses the AWS sdk to change the account ECR 
+        // scanning settings to enhanced.
+
+        // For now, scan on image push is set to true. This means that the image will be scanned
+        // for vulnerabilites every time it is pushed up to the ECR repo. With enhanced scanning,
+        // the image would be continously scanned for vulnerabilities.
+        // See https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-scanning-enhanced.html
+        imageScanOnPush: true,
+    });
+
+    this.repository = ecrRepository
+    this.repositoryOutput = new CfnOutput(this, 'ECR repository', { value: ecrRepository.repositoryName });
+  }
+}
