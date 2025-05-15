28f3558837cfa75601cc2334debed5cc179238e2
Cezar Rata <ceradev@amazon.com>
2025-05-15 16:43:46 +0000

    feat: codebuild stck
    Signed-off-by: Cezar Rata <ceradev@amazon.com>

---
+++ test/codebuild-stack.test.ts
@@ -0,0 +1,63 @@
+import * as cdk from 'aws-cdk-lib';
+import { Template, Match } from 'aws-cdk-lib/assertions';
+import { CodeBuildStack } from '../lib/codebuild-stack';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import { BuildImageOS } from '../lib/utils';
+
+describe('CodeBuildStack', () => {
+  test('synthesizes the way we expect', () => {
+    const app = new cdk.App();
+    const stack = new CodeBuildStack(app, 'TestStack', {
+        env: {
+            account: '123456789012',
+            region: 'us-east-1'
+        },
+        projectName: 'test-project',
+        region: 'us-west-2',
+        arch: 'x86_64_ubuntu',
+        amiSearchString: 'ubuntu*22.04*',
+        environmentType: codebuild.EnvironmentType.LINUX_EC2,
+        buildImageOS: BuildImageOS.LINUX,
+    });
+    const template = Template.fromStack(stack);
+
+    // Assert that the stack creates a CodeBuild project
+    template.hasResourceProperties('AWS::CodeBuild::Project', {
+      Name: 'test-project',
+      Environment: {
+        Type: 'LINUX_EC2',
+        ComputeType: 'BUILD_GENERAL1_MEDIUM',
+        Image: Match.stringLikeRegexp('ami-1234'),
+        PrivilegedMode: true
+      },
+      Source: {
+        Type: 'GITHUB',
+        Location: 'https://github.com/runfinch/finch.git',
+        ReportBuildStatus: true
+      }
+    });
+
+    // Assert that the stack creates a Fleet
+    template.hasResourceProperties('AWS::CodeBuild::Fleet', {
+      BaseCapacity: 1,
+      ComputeType: 'BUILD_GENERAL1_MEDIUM',
+      EnvironmentType: 'LINUX_EC2'
+    });
+
+    // Assert that the stack creates a KMS key
+    template.hasResourceProperties('AWS::KMS::Key', {
+      Description: 'Kms Key to encrypt data-at-rest',
+      Enabled: true
+    });
+
+    // Assert that the stack creates a KMS alias
+    template.hasResourceProperties('AWS::KMS::Alias', {
+      AliasName: Match.stringLikeRegexp('alias/finch-x86_64_ubuntu-kms-us-west-2')
+    });
+
+    // Check resource count
+    template.resourceCountIs('AWS::CodeBuild::Project', 1);
+    template.resourceCountIs('AWS::CodeBuild::Fleet', 1);
+    template.resourceCountIs('AWS::KMS::Key', 1);
+  });
+});
