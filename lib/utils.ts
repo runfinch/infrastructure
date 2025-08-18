b69e6260117dfe4f2de18983b0cf529fbad0d64c
ayush-panta <ayushkp@amazon.com>
2025-08-18 10:07:03 -0700

    ci: adjust runner number to true limit
    Signed-off-by: ayush-panta <ayushkp@amazon.com>

--- lib/utils.ts
+++ lib/utils.ts
@@ -54,7 +54,7 @@ export const CODEBUILD_STACKS: CodeBuildStackArgs[] = [
     buildImageString: codebuild.MacBuildImage.BASE_14,
     fleetProps: {
       computeType: codebuild.FleetComputeType.MEDIUM,
-      baseCapacity: 5 // ideally 8, but capped at 5
+      baseCapacity: 4 // ideally 8, but capped
     },
   }
 ];
