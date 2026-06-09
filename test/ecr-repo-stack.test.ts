e5f7b101aee7da29c3959f418171fdfed6c7b745
Swapnanil Gupta <swpnlg@amazon.com>
2026-06-09 10:52:36 -0700

    feat: add finch os build cache repo to ecr (#1103)
    Signed-off-by: Swapnanil-Gupta <swpnlg@amazon.com>

--- test/ecr-repo-stack.test.ts
+++ test/ecr-repo-stack.test.ts
@@ -10,14 +10,41 @@ describe('ECRRepositoryStack', () => {
     // prepare the ECRRepositoryStack template for assertions
     const template = Template.fromStack(ecrRepo);
 
-    // assert it creates the ecr repo with properties set.
-    template.resourceCountIs('AWS::ECR::Repository', 1);
+    // assert it creates the ecr repos with properties set.
+    template.resourceCountIs('AWS::ECR::Repository', 2);
     template.hasResource('AWS::ECR::Repository', {
       Properties: {
-        RepositoryName: Match.anyValue(),
+        RepositoryName: 'finch-rootfs-image-test',
         ImageTagMutability: "IMMUTABLE",
         ImageScanningConfiguration: {
           ScanOnPush: true,
+        },
+      },
+    });
+
+    template.hasResource('AWS::ECR::Repository', {
+      Properties: {
+        RepositoryName: 'finch-os-build-cache-test',
+        ImageTagMutability: "MUTABLE",
+        ImageScanningConfiguration: {
+          ScanOnPush: false,
+        },
+        LifecyclePolicy: {
+          LifecyclePolicyText: Match.serializedJson({
+            rules: [
+              {
+                rulePriority: 1,
+                selection: {
+                  tagStatus: 'untagged',
+                  countType: 'sinceImagePushed',
+                  countNumber: 30,
+                  countUnit: 'days',
+                },
+                action: { type: 'expire' },
+                description: 'Expire untagged build cache layers after 30 days',
+              },
+            ],
+          }),
         },
       },
     });
